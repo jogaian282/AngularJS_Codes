ff8b9c79ca011f7c56885fe68083e7649d09be96
KarthikJogaian <karthikjogaianpm@yahoo.in>
2016-09-27 15:30:19 +0530

    Using Factories injection takes place for quiz

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -1,6 +1,6 @@
 app.controller('quizCtrl',QuizController);
-QuizController.$inject=['quizMetrics'];
-function QuizController(quizMetrics){
+QuizController.$inject=['quizMetrics','dataService'];
+function QuizController(quizMetrics,dataService){
 	var vm = this;
 	vm.quizMetrics=quizMetrics;
 }
