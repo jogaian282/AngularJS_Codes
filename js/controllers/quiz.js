20bcbc718f9bd7c019f2e6952910697e538f5f77
KarthikJogaian <karthikjogaianpm@yahoo.in>
2016-09-28 21:41:54 +0530

    Added Controller Logic for quiz app

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -3,4 +3,31 @@ QuizController.$inject=['quizMetrics','dataService'];
 function QuizController(quizMetrics,dataService){
 	var vm = this;
 	vm.quizMetrics=quizMetrics;
+	vm.dataService=dataService;
+	vm.activeQuestion=0;
+	vm.questionAnswered=questionAnswered;
+	vm.setActiveQuestion=setActiveQuestion;
+
+	var numQuestionsAnswered=0;
+	function questionAnswered(){
+		var quizLength = dataService.quizQuestions.length;
+		if(dataService.quizQuestions[vm.activeQuestion].selected !== null){
+			numQuestionsAnswered++;
+			if(numQuestionsAnswered >= quizLength){
+				//finalise Quiz
+			}
+		}
+		vm.setActiveQuestion()
+	}
+	function setActiveQuestion(){
+		var breakOut = false;
+		var quizLength = dataService.quizQuestions.length-1;
+
+		while(!breakOut){
+			vm.activeQuestion = vm.activeQuestion < quizLength ? ++vm.activeQuestion:0;
+			if(dataService.quizQuestions[vm.activeQuestion].selected === null){
+				breakOut = true;
+			}
+		}
+	}
 }
